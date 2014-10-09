7137f2ba0783dbabedf2b2c00442e7bea13b441f
unknown <patte.wilhelm@googlemail.com>
2014-10-09 12:20:47 +0200

    'Initialisierung'

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	
+	url(r'^$', Index.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^weather/$', 'PythonWetter.views.get_weather_list', name='PythonWeather'),
 )
