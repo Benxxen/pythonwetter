822e153fb858353c7a9501f7fa0d3532091da11b
unknown <patte.wilhelm@googlemail.com>
2014-10-14 20:51:03 +0200

    Datenbanktest

--- inputdb.py
+++ inputdb.py
@@ -2,8 +2,16 @@ __author__ = 'patrick'
 import psycopg2
 import os
 
-connection = psycopg2.connect(os.environ['OPENSHIFT_APP_NAME'])
+connection = psycopg2.connect(database=os.environ['OPENSHIFT_APP_NAME'], user=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'], password=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'], host=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'], port=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'])
 cursor = connection.cursor()
 cursor.execute("INSERT INTO PythonWetter_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-14','Potsdam','Yahoo','Rainy',10,'C',30,60,'SW')")
 connection.commit()
 connection.close()
+
+
+
+            'NAME':     os.environ['OPENSHIFT_APP_NAME'],
+            'USER':     os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
+            'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
+            'HOST':     os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
+            'PORT':     os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
