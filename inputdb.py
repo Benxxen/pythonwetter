f74ff20b30eaf6a56470c69291815cb9c5b561a8
unknown <patte.wilhelm@googlemail.com>
2014-10-14 20:46:06 +0200

    Datenbanktest

--- inputdb.py
+++ inputdb.py
@@ -1,8 +1,8 @@
 __author__ = 'patrick'
-import sqlite3
+import psycopg2
 import os
 
-connection = sqlite3.connect(os.environ['OPENSHIFT_APP_NAME'])
+connection = psycopg2.connect(os.environ['OPENSHIFT_APP_NAME'])
 cursor = connection.cursor()
 cursor.execute("INSERT INTO PythonWetter_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-14','Potsdam','Yahoo','Rainy',10,'C',30,60,'SW')")
 connection.commit()
