12a8cf3bce0369a9d997e2bf9a6cdcd9fe7e1483
unknown <patte.wilhelm@googlemail.com>
2014-10-21 20:54:14 +0200

    Inputscript

--- inputdb.py
+++ inputdb.py
@@ -36,6 +36,6 @@ dom.getElementsByTagName('title')[0].firstChild.data
 
 connection = psycopg2.connect(os.environ['OPENSHIFT_APP_NAME'])
 cursor = connection.cursor()
-cursor.execute("INSERT INTO PythonWetter_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-21','Potsdam','Yahoo','%s',10,'C',30,60,'SW'),", ycondition.getAttribute('text'),)
+cursor.execute("INSERT INTO PythonWetter_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-21','Potsdam','Yahoo','test',10,'C',30,60,'SW'),",)
 connection.commit()
 connection.close()
