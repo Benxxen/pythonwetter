e459900d1151016587d818ad15571ba64b9f81d6
unknown <patte.wilhelm@googlemail.com>
2014-10-29 20:41:49 +0100

    Input ueberarbeitet

--- inputdb.py
+++ inputdb.py
@@ -1,17 +1,21 @@
+# -*- coding: UTF-8 -*-
 __author__ = 'patrick'
-import psycopg2
-import os
+from PythonWetter.models import Weather
 import yweather
 import urllib
 import hashlib
 from xml.dom import minidom
+from time import strftime
 from django.shortcuts import render
 
+city = 'Potsdam'
+
+########### Yahoo ###########
 WEATHER_URLY = 'http://xml.weather.yahoo.com/forecastrss?w=%s&u=c'
 WEATHER_NSY = 'http://xml.weather.yahoo.com/ns/rss/1.0'
 
 client = yweather.Client()
-woeid = client.fetch_woeid('Berlin')
+woeid = client.fetch_woeid(city)
 
 url = WEATHER_URLY % woeid
 dom = minidom.parse(urllib.urlopen(url))
@@ -21,19 +25,16 @@ yastronomy = dom.getElementsByTagNameNS(WEATHER_NSY, 'astronomy')[0]
 ywind = dom.getElementsByTagNameNS(WEATHER_NSY, 'wind')[0]
 yunits = dom.getElementsByTagNameNS(WEATHER_NSY, 'units')[0]
 
-windspeed = str(ywind.getAttribute('speed'))
-windspeed = int(windspeed[:-3])
+ywindspeed = str(ywind.getAttribute('speed'))
+ywindspeed = int(ywindspeed[:-3])
 
 winddir = int(ywind.getAttribute('direction'))
-unit = yunits.getAttribute('temperature'),
-yastronomy.getAttribute('sunrise'),
-temperature = ycondition.getAttribute('temp'),
-yastronomy.getAttribute('sunset'),
-condition = ycondition.getAttribute('text')
-ycondition.getAttribute('temp'),
-code = str(ycondition.getAttribute('code')),
-forecasts,
-dom.getElementsByTagName('title')[0].firstChild.data
+unit = yunits.getAttribute('temperature')
+temperature = ycondition.getAttribute('temp')
+condition = str(ycondition.getAttribute('text'))
+code = int(ycondition.getAttribute('code'))
+title = dom.getElementsByTagName('title')[0].firstChild.data
+
 
 if winddir <= 23:
     winddir = 'N'
@@ -53,9 +54,45 @@ elif winddir <= 338:
     winddir = 'NW'
 else:
     winddir = 'no'
+######################
 
-connection = psycopg2.connect(database='pythonwetter', user=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'], password=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'], host=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'], port=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'])
-cursor = connection.cursor()
-cursor.execute("INSERT INTO mysite_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-21','Berlin','Yahoo',%s,%s,%s,%s,%s,%s)", (condition, temperature, unit, code, windspeed, winddir ))
-connection.commit()
-connection.close()
+########### Wetter.com ###########
+projektname = "pythonwetterfhb"
+apikey = "c5aa08dea1427f7a5a90762ccca6d430"
+checksum = hashlib.md5(projektname + apikey + city).hexdigest()
+
+urlstart = "http://api.wetter.com/location/name/search/"
+cityURL = urlstart + city + "/project/" + projektname + "/cs/" + checksum
+url = cityURL
+dom = minidom.parse(urllib.urlopen(url))
+
+citycode = dom.getElementsByTagName('city_code')[0].firstChild.data
+client = yweather.Client()
+
+
+apiurl = "http://api.wetter.com/forecast/weather/city/"
+
+projektname = "pythonwetterfhb"
+apikey = "c5aa08dea1427f7a5a90762ccca6d430"
+checksum = hashlib.md5(projektname + apikey + citycode).hexdigest()
+url = apiurl + citycode + "/project/" + projektname + "/cs/" + checksum
+dom = minidom.parse(urllib.urlopen(url))
+
+
+wwindspeed = dom.getElementsByTagName('ws')[4].firstChild.data
+wwinddir = dom.getElementsByTagName('wd_txt')[4].firstChild.data
+wsunrise = 'keine Angabe'
+wsunset = 'keine Angabe'
+wtemp = 'keine Angabe'
+wcode = dom.getElementsByTagName('w')[4].firstChild.data
+wtitle = dom.getElementsByTagName('name')[0].firstChild.nodeValue + ", " + citycode[0:2]
+wcondition = dom.getElementsByTagName('w_txt')[4].firstChild.data
+wtemperature = dom.getElementsByTagName('tx')[4].firstChild.data
+
+w = Weather(datum=strftime("%Y-%m-%d"), stadt=title, anbieter='Yahoo', wetter=condition, tagestemperatur=temperature,
+            einheit=unit, kondition=code, windgeschwindigkeit=ywindspeed, windrichtung=winddir)
+w.save()
+
+w = Weather(datum=strftime("%Y-%m-%d"), stadt=wtitle, anbieter='Wetter.com', wetter=wcondition, tagestemperatur=wtemperature,
+            einheit=unit, kondition=wcode, windgeschwindigkeit=wwindspeed, windrichtung=wwinddir)
+w.save()
---
+++ inputdb_alt.py
@@ -0,0 +1,61 @@
+__author__ = 'patrick'
+import psycopg2
+import os
+import yweather
+import urllib
+import hashlib
+from xml.dom import minidom
+from django.shortcuts import render
+
+WEATHER_URLY = 'http://xml.weather.yahoo.com/forecastrss?w=%s&u=c'
+WEATHER_NSY = 'http://xml.weather.yahoo.com/ns/rss/1.0'
+
+client = yweather.Client()
+woeid = client.fetch_woeid('Berlin')
+
+url = WEATHER_URLY % woeid
+dom = minidom.parse(urllib.urlopen(url))
+forecasts = []
+ycondition = dom.getElementsByTagNameNS(WEATHER_NSY, 'condition')[0]
+yastronomy = dom.getElementsByTagNameNS(WEATHER_NSY, 'astronomy')[0]
+ywind = dom.getElementsByTagNameNS(WEATHER_NSY, 'wind')[0]
+yunits = dom.getElementsByTagNameNS(WEATHER_NSY, 'units')[0]
+
+windspeed = str(ywind.getAttribute('speed'))
+windspeed = int(windspeed[:-3])
+
+winddir = int(ywind.getAttribute('direction'))
+unit = yunits.getAttribute('temperature'),
+yastronomy.getAttribute('sunrise'),
+temperature = ycondition.getAttribute('temp'),
+yastronomy.getAttribute('sunset'),
+condition = ycondition.getAttribute('text')
+ycondition.getAttribute('temp'),
+code = str(ycondition.getAttribute('code')),
+forecasts,
+dom.getElementsByTagName('title')[0].firstChild.data
+
+if winddir <= 23:
+    winddir = 'N'
+elif winddir <= 67:
+    winddir = 'NE'
+elif winddir <= 113:
+    winddir = 'E'
+elif winddir <= 158:
+    winddir = 'SE'
+elif winddir <= 203:
+    winddir = 'S'
+elif winddir <= 248:
+    winddir = 'SW'
+elif winddir <= 293:
+    winddir = 'W'
+elif winddir <= 338:
+    winddir = 'NW'
+else:
+    winddir = 'no'
+
+connection = psycopg2.connect(database='pythonwetter', user=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'], password=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'], host=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'], port=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'])
+cursor = connection.cursor()
+cursor.execute("INSERT INTO mysite_weather (Datum, Stadt, Anbieter, Wetter, Tagestemperatur, Einheit, Kondition, Windgeschwindigkeit, Windrichtung) VALUES ('2014-10-21','Berlin','Yahoo',%s,%s,%s,%s,%s,%s)", (condition, temperature, unit, code, windspeed, winddir ))
+connection.commit()
+connection.close()
