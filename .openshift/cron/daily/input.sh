67cebeef3458575c23978e1cd8b8d181e3040b18
unknown <patte.wilhelm@googlemail.com>
2014-11-02 21:34:29 +0100

    11:00

--- .openshift/cron/daily/input.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python ~/app-root/repo/manage.py shell < ~/app-root/repo/inputdb.py
--- .openshift/cron/minutely/input.sh
+++ .openshift/cron/minutely/input.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ `date +%H:%M` == "14:18" ]
+if [ `date +%H:%M` == "05:00" ]
 then
-    bash python ~/app-root/repo/manage.py shell < ~/app-root/repo/inputdb.py
+    python ~/app-root/repo/manage.py shell < ~/app-root/repo/inputdb.py
 fi
