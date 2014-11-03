7f09666b19cf7f1fe55f1dea5b75469aa46d1499
unknown <patte.wilhelm@googlemail.com>
2014-11-03 18:49:25 +0100

    REST

--- pythonwetter/urls.py
+++ pythonwetter/urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^weather/$', 'PythonWetter.views.get_weather_list', name='PythonWeather'),
+    url(r'^weather/$', 'pythonwetter.views.get_weather_list', name='PythonWeather'),
 )
