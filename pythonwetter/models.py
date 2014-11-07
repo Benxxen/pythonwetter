d88fcbbd27575a666a0845e5da9d9d76647adaaf
unknown <patte.wilhelm@googlemail.com>
2014-11-07 12:11:52 +0100

    Bugfix

--- pythonwetter/models.py
+++ pythonwetter/models.py
@@ -17,33 +17,5 @@ class Weather(models.Model):
     windgeschwindigkeit = models.IntegerField(max_length=5)
     windrichtung = models.CharField(max_length=2)
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return str(self.datum) + ", " + self.stadt + ", " + self.anbieter
-# class PLZ(models.Model):
-#
-
-# class Forecast(models.Model):
-#     day = models.CharField(max_length=8)
-#     date = models.CharField(max_length=32)
-#     templow = models.IntegerField(max_length=8)
-#     temphigh = models.IntegerField(max_length=8)
-#     wetter = models.CharField(max_length=32)
-#     code = models.IntegerField(max_length=2)
-#
-#     def __str__(self):
-#         return "{} {} {}".format(self.wetter, self.city, self.temperaturemax, self.temperaturemin)
-
-
-    #location_region = models.CharField(max_length=10)
-    #location_country = models.CharField(max_length=32)
-    #units_distance = models.CharField(max_length=2)
-    #units_pressure = models.CharField(max_length=2)
-    #units_speed = models.CharField(max_length=4)
-    #wind_chill = models.IntegerField(max_length=8)
-
-    #astronomy_sunrise = models.CharField(max_length=8)
-    #astronomy_sunset = models.CharField(max_length=8)
-
-    #condition_temp = models.CharField(max_length=3)
-    #condition_date = models.CharField(max_length=20)
-
